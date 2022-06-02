c9f040808b76450a1beb8d40466b340ac74128b6
ajithK24 <ajithkumar@jobuli.in>
2022-06-02 15:45:47 +0530

    ui changes remove down cmp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { DownloadCertificateComponent } from './download-certificate/download-certificate.component';
 import { NgModule } from '@angular/core';
 import { UploadCertificateComponent } from './upload-certificate/upload-certificate.component';
 import { environment } from '../environments/environment';
 
 const routes: Routes = [
-  { path: "", component: UploadCertificateComponent },
-  { path: "download-certificate", component: DownloadCertificateComponent },
+  { path: "", component: UploadCertificateComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
-import { DownloadCertificateComponent } from './download-certificate/download-certificate.component';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { UploadCertificateComponent } from './upload-certificate/upload-certificate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadCertificateComponent,
-    DownloadCertificateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
